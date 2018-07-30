2896dd95c46499a725fb5f4c119e3e862eca236d
m4rkzg <39007717+m4rktn@users.noreply.github.com>
2018-07-30 21:12:14 +0200

    Add files via upload

---
+++ tooca.py
@@ -0,0 +1,171 @@
+import random 
+import string
+
+import  re, os
+from platform import system
+from random import choice
+if os.name == 'nt':
+	os.system('cls')
+else:
+	os.system('clear')
+def steam() :
+ print("""  Steam Generator (c)M4rkwalker """)
+ try :
+   for az in range(1000) :
+    urname = string.ascii_letters + string.digits
+    urname1 = ''.join(choice(urname) for _ in range(5))
+    taki = string.ascii_letters + string.digits
+    taki2 = ''.join(choice(taki) for _ in range(5))
+    metsuha = string.ascii_letters + string.digits
+    metsuha3 = ''.join(choice(metsuha) for _ in range(5)) 
+    mark = urname1.upper() +('-')+ taki2.upper() +('-')+ metsuha3.upper()
+    print(az,")",mark," by M4rk")
+    save = open("steam.txt", 'a')
+    save.write(mark + '\n')
+ except : 
+	   	 pass
+def hma() :
+ print("""  Hidemyass Generator (c)M4rkwalker """)
+ try :
+   for az in range(1000) :
+    urname = string.ascii_letters + string.digits
+    urname1 = ''.join(choice(urname) for _ in range(6))
+    taki = string.ascii_letters + string.digits
+    taki2 = ''.join(choice(taki) for _ in range(6))
+    metsuha = string.ascii_letters + string.digits
+    metsuha3 = ''.join(choice(metsuha) for _ in range(6)) 
+    mark = urname1.upper() +('-')+ taki2.upper() +('-')+ metsuha3.upper()
+    print(az,")",mark," by M4rk")
+    save = open("hma.txt", 'a')
+    save.write(mark + '\n')
+ except : 
+	   	 pass
+def g2a() :
+ print("""  G2A Generator (c)M4rkwalker """)
+ try :
+   for az in range(1000) :
+    urname = string.ascii_letters + string.digits
+    urname1 = ''.join(choice(urname) for _ in range(4))
+    taki = string.ascii_letters + string.digits
+    taki2 = ''.join(choice(taki) for _ in range(4))
+    metsuha = string.ascii_letters + string.digits
+    metsuha3 = ''.join(choice(metsuha) for _ in range(4)) 
+    mark = urname1.upper() +('-')+ taki2.upper() +('-')+ metsuha3.upper()
+    print(az,")",mark," by M4rk")
+    save = open("g2a.txt", 'a')
+    save.write(mark + '\n')
+ except : 
+	   	 pass
+	   	
+def gplay() :
+ print("""  Googleplay Generator (c)M4rkwalker """)
+ try :
+   for az in range(1000) :
+    urname = string.ascii_letters + string.digits
+    urname1 = ''.join(choice(urname) for _ in range(4))
+    taki = string.ascii_letters + string.digits
+    taki2 = ''.join(choice(taki) for _ in range(4))
+    metsuha = string.ascii_letters + string.digits
+    metsuha3 = ''.join(choice(metsuha) for _ in range(4))
+    miki = string.ascii_letters + string.digits
+    miki3 = ''.join(choice(miki) for _ in range(4))
+    tsukaza = string.ascii_letters + string.digits
+    tsukaza3 = ''.join(choice(tsukaza) for _ in range(4))
+    mark = urname1.upper() +('-')+ taki2.upper() +('-')+ metsuha3.upper() +('-')+ miki3.upper() +('-')+ tsukaza3.upper()
+    print(az,")",mark," by M4rk")
+    save = open("gplay.txt", 'a')
+    save.write(mark + '\n')
+ except : 
+	   	 pass
+def express() :
+ print("""  Express Vpn Generator (c)M4rkwalker """)
+ try :
+   for az in range(1000) :
+    urname = string.ascii_letters + string.digits
+    urname1 = ''.join(choice(urname) for _ in range(22))
+    print(az,")","E"+ urname1.upper()," by M4rk")
+    save = open("express.txt", 'a')
+    save.write(urname1 + '\n')
+ except : 
+	   	 pass
+def karta() :
+ print("""  7ammass Generator (c)M4rkwalker """)
+ try :
+   for az in range(1000) :
+    gothar = string.digits
+    gothar69 = ''.join(choice(gothar) for _ in range(14))
+    print(az,")",gothar69," by M4rk")
+    save = open("karta.txt", 'a')
+    save.write(gothar69 + '\n')
+ except : 
+	   	 pass
+def abo():
+ print("""
+ Name : Zakarya
+ Nickname : M4rkWalker
+ Counrty : TN
+ Facebook : https://fb.com/officiel.zack
+ Github : https://github.com/m4rktn
+ Youtube : M4rkWalker """)
+ exit()
+ 
+def main():
+ logo = ("""
+
+                  `+@.          
+              `'@#++++#@#`      
+             ,@++++++++++@;     
+            +#+++++#@#++++#@`   
+           '+++++++@@+++++++@   
+          ,#++++++@@@#++++++##  
+         `@+++++++@@@@+++++++@` 
+         '+++++++++@@+++++++++# 
+         @++++++++++++++++++++@ 
+         #++++++++++++++++++++#@
+         +++++++++@@@@+++++++++@     
+        '+++++++++@@@@+++++++++@    
+        ++++++++++@@@@+++++++++@   
+        `+++++@#++@@@@++#@+++++@ 
+         #++#@@@#++++++#@@@+++#@   
+         @++@@@@#++++++#@@@#++@`      
+         ++++@@@++++++++#@@#++@        
+         `@+++++++++++++++@++@:   #Keys Generator      
+          ;#+++++++++++++#++#@   #Coded By  
+           @++++++++++++++++@   #M4rkWalker
+           `@#++++++++++++#@.  #zakarya Addala 
+             +@++++++++++@@   #Keys Generator
+              `#@#++++#@@:   #FB.com/officiel.zack
+                 @@@@@@`    #Email:Dream0@protonmail.com
+ """)
+ print(logo)
+ print(" [1] Steam ")
+ print(" [2] Express ")
+ print(" [3] Hidemyass ")
+ print(" [4] Tn Mobile Sold ")
+ print(" [5] G2A ")
+ print(" [6] Google Play ")
+ print(" [7] Methods ")
+ print(" [69] About & Exit ")
+
+ zack = input("$ ")
+ if zack == '1':
+     steam()
+ elif zack == '2':
+    express()
+ elif zack == '3' :
+    hma()
+ elif zack == '4' :
+    karta()
+ elif zack == '5' :
+    g2a()
+ elif zack == '6' :
+    gplay()
+ elif zack == '7' :
+    print (""" For Next Time Nchallah ^_^ """)
+    exit()
+ elif zack == '69' :
+    abo()
+ else :
+     print(" Open Ur Eyes !! Bakka -____- ")
+     exit()
+main() 
